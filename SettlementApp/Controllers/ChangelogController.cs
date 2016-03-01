c4bd35aaf10e67254bfa12bf018fc5dd1d25495e
lukas.wilkes <lukas.wilkes@lichtblick.de>
2016-03-01 18:49:48 +0100

    Submit beim Login, Changelg Seite

---
+++ SettlementApp/Controllers/ChangelogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SettlementApp.Controllers
+{
+    public class ChangelogController : Controller
+    {
+        // GET: Changelog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
